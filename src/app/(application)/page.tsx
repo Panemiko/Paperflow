09201282981447d25afdeccc88c172a1bb8f86a2
Tito Oliveira <titoluccao@gmail.com>
2025-02-20 11:08:25 -0300

    feat: restart development with t3-app

---
+++ src/app/(application)/page.tsx
@@ -0,0 +1,9 @@
+import TailwindAdvancedEditor from "@/components/editor/advanced-editor";
+
+export default async function Page() {
+  return (
+    <div className="bg-accent min-h-screen w-full">
+      <TailwindAdvancedEditor />
+    </div>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+import { TRPCReactProvider } from "@/trpc/react";
+
+export const metadata: Metadata = {
+  title: "Paperflow",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="pt-BR" className={`${GeistSans.variable}`}>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
