20ad9296972abc810799fcdd3987fcaf06b17609
Tito Lucca Oliveira <titoluccao@gmail.com>
2025-03-14 19:17:31 -0300

    feat: upgrade tailwind version to v4

--- src/app/(application)/page.tsx
+++ src/app/(application)/page.tsx
@@ -1,9 +1,3 @@
-import TailwindAdvancedEditor from "@/components/editor/advanced-editor";
-
 export default async function Page() {
-  return (
-    <div className="bg-accent min-h-screen w-full">
-      <TailwindAdvancedEditor />
-    </div>
-  );
+  return <div className="bg-accent min-h-screen w-full">teste</div>;
 }
